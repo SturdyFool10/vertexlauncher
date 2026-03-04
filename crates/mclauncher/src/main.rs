717196803c7ef6d8d99e55a18fab8cabdb134d5b
SturdyFool10 <sturdy@sturdyfool10.com>
2026-03-04 00:09:53 -0700

    remove the cosmic-text dep and glue, it would be real nice to use but it won't work as well as I want... getting ready for real app dev

--- crates/mclauncher/src/main.rs
+++ crates/mclauncher/src/main.rs
@@ -1,3 +1,65 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::{self, egui};
+use egui::CentralPanel;
+
+struct VertexApp {}
+
+impl VertexApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self {}
+    }
+}
+
+impl eframe::App for VertexApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| ui.label("Hello Vertex Launcher"));
+    }
+}
+
+fn main() -> eframe::Result<()> {
+    let options: eframe::NativeOptions = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder {
+            title: Some("Vertex Launcher".into()),
+            inner_size: Some(egui::vec2(1280.0, 800.0)),
+            min_inner_size: Some(egui::vec2(320.0, 240.0)),
+            resizable: Some(true),
+            ..Default::default()
+        },
+        renderer: eframe::Renderer::Wgpu,
+        hardware_acceleration: eframe::HardwareAcceleration::Required,
+        vsync: false,
+        multisampling: 1,
+        depth_buffer: 0,
+        stencil_buffer: 0,
+        dithering: false,
+        centered: false,
+        persist_window: false,
+        event_loop_builder: None,
+        window_builder: None,
+        shader_version: None,
+        run_and_return: false,
+        wgpu_options: eframe::egui_wgpu::WgpuConfiguration {
+            present_mode: eframe::egui_wgpu::wgpu::PresentMode::AutoNoVsync,
+            desired_maximum_frame_latency: Some(1),
+            wgpu_setup: eframe::egui_wgpu::WgpuSetup::CreateNew(
+                eframe::egui_wgpu::WgpuSetupCreateNew {
+                    instance_descriptor: eframe::egui_wgpu::wgpu::InstanceDescriptor {
+                        backends: eframe::egui_wgpu::wgpu::Backends::VULKAN,
+                        ..Default::default()
+                    },
+                    power_preference: eframe::egui_wgpu::wgpu::PowerPreference::LowPower,
+                    ..Default::default()
+                },
+            ),
+            on_surface_error: std::sync::Arc::new(|_| {
+                eframe::egui_wgpu::SurfaceErrorAction::RecreateSurface
+            }),
+        },
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Vertex Launcher",
+        options,
+        Box::new(|cc| Ok(Box::new(VertexApp::new(cc)))),
+    )
 }
